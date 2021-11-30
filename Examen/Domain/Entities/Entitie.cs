356b44d0fdf3d39b98666b77368e207a7802d449
rony-obando <ronyalvertoobando19092003@gmail.com>
2021-11-30 14:13:08 -0600

    Subiendo estructura

---
+++ Examen/Domain/Entities/Entitie.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Entities
+{
+    public class Entitie
+    {
+        public string a { get; set; }
+        public int b { get; set; }
+        public decimal c { get; set; }
+    }
+}
